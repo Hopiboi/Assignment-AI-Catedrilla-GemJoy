f3372e37cd07415e9118613c2e6c9011b222a456
Hopiboi <89178015+Hopiboi@users.noreply.github.com>
2022-03-19 22:01:41 +0800

    Assignment Illusionary AI

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);  
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/static/index.html');
+});
+
+server.listen(3000, function() {
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket) {
+    
+    socket.on('move', function(msg){
+        socket.broadcast.emit('move',msg)
+
+
+    })
+
+
+});
